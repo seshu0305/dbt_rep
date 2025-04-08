87dacc9125bff06a7f1076700f1518e5286ee761
seshubabu435@gmail.com <seshubabu435@gmail.com>
2025-04-08 21:40:22 +0000

    updated models

---
+++ models/example/get_date_parts.sql
@@ -0,0 +1,12 @@
+{% macro get_date_parts(date_column) %}
+  OBJECT_CONSTRUCT(
+    'original_date', {{ adapter.quote(date_column) }},
+    'year', YEAR(CAST({{ date_column }} AS DATE)),
+    'month', MONTH(CAST({{ date_column }} AS DATE)),
+    'day', DAY(CAST({{ date_column }} AS DATE)),
+    'day_of_week', DAYOFWEEK(CAST({{ date_column }} AS DATE)),
+    'day_of_year', DAYOFYEAR(CAST({{ date_column }} AS DATE)),
+    'week', WEEK(CAST({{ date_column }} AS DATE)),
+    'quarter', QUARTER(CAST({{ date_column }} AS DATE))
+  )
+{% endmacro %}
